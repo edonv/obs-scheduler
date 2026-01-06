b7de416f9ad571f0e81ab2a8194f0e58ecee5e4d
Edon <edon@valdman.works>
2026-01-06 12:18:40 -0600

    Create obs.js
    Added file with logic for creating `RequestBatchRequest`s from events.

---
+++ dist/obs.js
@@ -0,0 +1,64 @@
+/** @typedef {import('./config.js').ConfigEvent} ConfigEvent */
+
+/**
+ * Maps a `ConfigEvent` to an OBS Websocket request.
+ * @param {ConfigEvent} event 
+ * @returns {import("obs-websocket-js").RequestBatchRequest[]}
+ */
+export function convertEventToWebsocketRequest(event) {
+    const requestID = `${event.name ? event.name + '.' : ''}${event.action}`;
+
+    switch (event.action) {
+        case "start_stream":
+            return [{
+                requestID,
+                requestType: 'StartStream',
+            }];
+        case "stop_stream":
+            return [{
+                requestID,
+                requestType: 'StopStream',
+            }];
+        case "start_recording":
+            return [{
+                requestID,
+                requestType: 'StartRecord',
+            }];
+        case "pause_recording":
+            return [{
+                requestID,
+                requestType: 'PauseRecord',
+            }];
+        case "stop_recording":
+            return [{
+                requestID,
+                requestType: 'StopRecord',
+            }];
+        case "advanced_scene_switcher":
+            if (event.switcher_message) {
+                return [{
+                    requestID,
+                    requestType: 'CallVendorRequest',
+                    requestData: {
+                        requestData: {
+                            message: event.switcher_message,
+                        },
+                        requestType: "AdvancedSceneSwitcherMessage",
+                        vendorName: "AdvancedSceneSwitcher",
+                    },
+                }];
+            } else {
+                return [];
+            }
+        case "obs_ws":
+            if (event.ws_request) {
+                return [{
+                    requestID,
+                    requestType: event.ws_request.type,
+                    requestData: event.ws_request.data,
+                }];
+            } else {
+                return [];
+            }
+    }
+}
